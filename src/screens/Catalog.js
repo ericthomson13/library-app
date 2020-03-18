222f8b357f4f5470170b918099aab6cb595d64ea
et <eric.thomson13@gmail.com>
2020-03-18 15:43:57 -0600

    Added Shuffle and Catalog List

--- src/screens/Catalog.js
+++ src/screens/Catalog.js
@@ -2,10 +2,35 @@ import React, { useState } from 'react';
 
 import CatalogItem from '../components/CatalogItem';
 
+const shuffle = (array) => {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+};
 const Catalog = (props) => {
+  const catalogItems = [
+    'PS3022 .K622 2019', 'QA890 .T92 2018', 'QA890 .T92 2019', 'Q690 .S12.2016', 'PQ3022 .N84 2019', 'PS3022 .K601 2019'
+  ];
+  const shuffledItems = shuffle([...catalogItems]);
 
   return (
     <h2>Catalog Page</h2>
+
   );
 };
 
