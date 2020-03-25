ea203b10cec4750bf346347bfaac5e4995413ae3
et <eric.thomson13@gmail.com>
2020-03-25 11:03:55 -0600

    Buttons and Handlers Added for MLA

--- src/components/MLAItem.js
+++ src/components/MLAItem.js
@@ -1,17 +1,50 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect, } from 'react';
 
 import { shuffle, } from '../utility/functions';
 
 const MLAItem = ({ type, item, }) => {
   const [shuffledItem, setShuffledItem] = useState(shuffle([ ...item ]));
+  const [winner, setWinner] = useState(false);
 
-  const displayedList = shuffledItem.map((string) => (
-    <span>{string}</span>
-  ))
+  useEffect(() => {
+    for (let i = 0; i < shuffledItem.length ; i++) {
+      if (shuffledItem[i] !== item[i]) return;
+    }
+    setWinner(true);
+  }, [shuffledItem, item]);
+
+  const upHandler = (i) => {
+    if (i === 0) return;
+    const newArr = [...shuffledItem];
+    const temp = newArr[i];
+    newArr[i] = newArr[i - 1];
+    newArr[i - 1] = temp;
+    setShuffledItem(newArr);
+  };
+
+  const downHandler = (i) => {
+    if (i === shuffledItem.length) return;
+    const newArr = [...shuffledItem];
+    const temp = newArr[i];
+    newArr[i] = newArr[i + 1];
+    newArr[i + 1] = temp;
+    setShuffledItem(newArr);
+  };
+
   return (
     <>
       <h3>{type}</h3>
-      <p>{displayedList}</p>
+      <p>{
+        shuffledItem.map((string, i) => (
+          <>
+            <button type="button" onClick={() => upHandler(i)}>Up</button>
+            <span>{string}</span>
+            <button type="button" onClick={() => downHandler(i)}>Down</button>
+          </>
+        ))
+      }</p>
     </>
   );
 };
