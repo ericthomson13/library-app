86021774286becea93124727b4b7023f7a604d36
et <eric.thomson13@gmail.com>
2020-03-25 10:57:11 -0600

    MLA Strings Rendering Shuffled

--- src/components/MLAItem.js
+++ src/components/MLAItem.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect, } from 'react';
 
-import { shuffle } from '../utility/functions';
+import { shuffle, } from '../utility/functions';
 
 const MLAItem = ({ type, item, }) => {
-  
+  const [shuffledItem, setShuffledItem] = useState(shuffle([ ...item ]));
+
+  const displayedList = shuffledItem.map((string) => (
+    <span>{string}</span>
+  ))
   return (
-    <div>This will be an MLA Item</div>
+    <>
+      <h3>{type}</h3>
+      <p>{displayedList}</p>
+    </>
   );
 };
 
