f1e65ae824a6d82c8ad6d3cddf41e4caa57cf9e0
et <eric.thomson13@gmail.com>
2020-03-25 15:13:04 -0600

    APA CitationItem Testing Added

--- src/tests/CitationItem.test.js
+++ src/tests/CitationItem.test.js
@@ -4,14 +4,15 @@ import { mount } from 'enzyme';
 import CitationItem from '../components/CitationItem';
 
 import mlaData from '../utility/mlaData';
+import apaData from '../utility/apaData';
 
-describe('MLAItem Unit Tests', () => {
+describe('MLA CitationItem Unit Tests', () => {
   let mountTest;
   beforeEach(() => {
     mountTest = mount(<CitationItem type={mlaData[0].type} item={[ ...mlaData[0].items ]} />);
   });
   it('Renders without Crashing', () => {
-    expect(mountTest.find('.mla-item').length).toBe(1);
+    expect(mountTest.find('.citation-item').length).toBe(1);
   });
   it('Does not Render Winner on Mount', () => {
     expect(mountTest.find('.winner').length).toBe(0);
@@ -20,4 +21,17 @@ describe('MLAItem Unit Tests', () => {
   xit('Renders Winner when Arrays Match', () => {
     expect(mountTest.find('.winner').hasClass('winner')).toBe(true);
   })
+});
+
+describe('APA CitationItem Unit Tests', () => {
+  let mountTest;
+  beforeEach(() => {
+    mountTest = mount(<CitationItem type={apaData[0].type} item={[ ...apaData[0].item ]} />);
+  });
+  it('Renders without Crashing', () => {
+    expect(mountTest.find('.citation-item').length).toBe(1);
+  });
+  it('Does not Render Winner on Mount', () => {
+    expect(mountTest.find('.winner').length).toBe(0);
+  });
 });
