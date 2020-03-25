651815c28cdf8f86828e8fec98148915e5cb9e78
et <eric.thomson13@gmail.com>
2020-03-25 10:41:21 -0600

    MLA Data Added

---
+++ src/utility/mlaData.js
@@ -0,0 +1,34 @@
+const mlaData = [
+  {
+    type: 'Journal Article, Accessed Online',
+    items: [
+      'Zambrano, Patrizia.', 'Sandro Botticelli and the Birth of Modern Portraiture.', ' Botticelli Past and Present,', 'edited by Ana Debenedetti and Caroline Elam,', 'UCL Press,', 'London,', '2019,', 'pp. 10–35.', 'JSTOR,', 'www.jstor.org/stable/j.ctv550cgj.7.', 'Accessed March 5th 2020.',
+    ],
+  },
+  {
+    type: 'Journal Article, Accessed Online',
+    items: [
+      'Price, C. Matlack. ', 'Italian Derivations in American Architecture: The Revival of Architectural Ideals of The Renaissance. ', 'Arts & Decoration (1910-1918), ', 'vol. 3, ', 'no. 7, ', '1913, ', 'pp. 240–243. ',  'JSTOR, ', 'www.jstor.org/stable/43805843. ', 'Accessed 5 Mar. 2020.',
+    ],
+  },
+  {
+    type: 'Journal Article',
+    items: [
+      'Matteson, Lynn R. ', 'The Poetics and Politics of Alpine Passage: Turner’s Snowstorm: Hannibal and His Army Crossing the Alps. ', 'The Art Bulletin, ', 'vol. 62, ', 'no. 3, ', '1980, ', 'pp. 385–98.',
+    ]
+  },
+  {
+    type: 'Book with Multiple Authors',
+    items: [
+      'Dürer, Albrecht, ', 'and Troutman, Philip. ', 'Albrecht Dürer: Sketchbook of His Journey to the Netherlands 1520-21. ', 'Praeger, ', '1971.',
+    ],
+  },
+  {
+    type: 'Book',
+    items: [
+      'Reed, James Stalford. ', 'Principles of ceramics processing. ', '2nd ed., ', 'Wiley, ', '1995.',
+    ],
+  },
+];
+
+export default mlaData;
