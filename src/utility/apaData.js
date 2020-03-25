a88b357e12c2f919761e383f6da46af008d7277e
et <eric.thomson13@gmail.com>
2020-03-25 14:52:49 -0600

    apaData Addeed

---
+++ src/utility/apaData.js
@@ -0,0 +1,37 @@
+const apaData = [
+  {
+    id: 123124321,
+    type: 'Article with Multiple Authors',
+    item: [
+      'Williams, M., Villarreal, A., Bozhilov, K., Lin, S.,', ' & Talbot, P., ', '(2013). ', 'Metal and silicate particles including nanoparticles are present in electronic cigarette cartomizer fluid aerosol. ', 'PloS One, ', '8(3), 1-11. ', 'https://doi.org/10.1371/journal.pone.0057987',
+    ],
+  },
+  {
+    id: 3417392561,
+    type: 'Article with Multiple Authors Accessed Online',
+    item: [
+      'Callen, S. T., & Miller, A. J. ', '(2015). ', 'Signatures of niche conservatism and niche shift in the North American kudzu (Pueraria montana) invasion. ', 'Diversity and Distributions, ', '21(7/8), ', '853–863. ', 'JSTOR.',
+    ],
+  },
+  {
+    id: 19723467767,
+    type: 'Article with a Single Author, Accessed Online',
+    item: [
+      'Alexander, P.A. ', '(2018). ', 'Past as prologue: educational psychology\'s legacy and progeny. ', 'Journal of Educational Psychology, ', '10(2), ', '147-162. ', 'https://doi.org/10.1037/edu0000200',
+    ],
+  },
+  {
+    id: 9871657349,
+    type: 'Article with Multiple Authors, Accessed Online',
+    item: [
+      'Pitman, G. M., Flockhart, D. T. T., ', '& Norris, D. R. ', '(2018). ', 'Patterns and causes of oviposition in monarch butterflies: Implications for milkweed restoration. ', 'Biological Conservation, ', '217, ', '54–65. ', 'https://doi.org/10.1016/j.biocon.2017.10.019',
+    ],
+  },
+  {
+    id: 81740348,
+    type: 'Article with Multiple Authors, Accessed Online',
+    item: [
+      'Flaherty, Kelley Lynn, Turk, Philip James, ', '& Anderson, James Thomas. ', '(2019). ', 'Comparing Stakeholder Attitudes Toward White-tailed Deer and Rare Plant Management in Canaan Valley, West Virginia. ', 'Global Ecology and Conservation, ', '17, ', 'e00519. ', 'https://doi.org/10.1016/j.gecco.2018.e00519',
+    ],
+  }
+]
